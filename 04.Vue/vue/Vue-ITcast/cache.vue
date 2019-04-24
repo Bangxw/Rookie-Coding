21d5cbfc1be6119d655f21792ebdb05592de9395
bangxw <wangbx@wewins.com>
2019-04-24 18:06:14 +0800

    update vue comment

--- 04.Vue/vue/Vue-ITcast/cache.vue
+++ 04.Vue/vue/Vue-ITcast/cache.vue
@@ -1,56 +1,96 @@
 <!DOCTYPE html>
-
-<html>
+<html lang='en'>
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatibal" content="ie=edge">
-  <title>Hello World!</title>
+  <meta charset='UTF-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+  <title>Document</title>
   <style>
-    #ball {
-      width: 30px;
-      height: 30px;
-      border-radius: 50%;
-      background: #f00;
+    html,
+    body {
+      margin: 0;
+      padding: 0;
     }
 
-    [v-cloak] {
-      display: none
+    h1 {
+      margin: 0;
+      padding: 0;
+    }
+
+    .container {
+      display: flex;
+      height: 500px;
+    }
+
+    .header-box {
+      background: lightgreen;
+      height: 70px;
+    }
+
+    .left-box {
+      flex: 2;
+      background: lightcyan
+    }
+
+    .main-box {
+      flex: 8;
+      background: lightblue
     }
   </style>
 </head>
 
 <body>
-  <div id="app" v-cloak>
-    <input type="button" value="加入购物车" @click="flag=!flag">
-    <transition @before-enter="beforeEnter" @after-enter="afterEnter" @enter="enter">
-      <div id="ball" v-if="flag"></div>
-    </transition>
+  <div id='app'>
+    <router-view></router-view>
+    <div class="container">
+      <router-view name="left"></router-view>
+      <router-view name="main"></router-view>
+    </div>
   </div>
 
-  <script src="../../lib/vue.js"></script>
+  <script src='../../lib/vue.js'></script>
+  <script src="../../lib/vue-router.js"></script>
   <script>
+    let header = {
+      template: '<h1 class="header-box">Header</h1>'
+    }
+
+    let left = {
+      template: '<h1 class="left-box">left</h1>'
+    }
+
+    let main = {
+      template: '<h1 class="main-box">main</h1>'
+    }
+
+    let router = new VueRouter({
+      routes: [{
+          path: '/',
+          components: {
+            default: 　header,
+            left,
+            main
+          }
+        }
+        // {
+        //   path: '/',
+        //   component: header
+        // }, {
+        //   path: '/left',
+        //   component: left
+        // }, {
+        //   path: '/main',
+        //   component: main
+        // }
+      ]
+    })
+
     new Vue({
       el: '#app',
-      data: {
-        flag: false
-      },
-      methods: {
-        beforeEnter(el) {
-          el.style.transform = "translate(0,0)"
-        },
-        enter(el, done) {
-          el.offsetWidth
-
-          el.style.transform = "translate(200px,350px)"
-          el.style.transition = "all 1.6s linear"
-          done()
-        },
-        afterEnter(el) {
-          this.flag = false
-        }
-      }
+      data: {},
+      methods: {},
+      router
     })
   </script>
 </body>
--- 06.Others/VsCode-HotKey.md
+++
@@ -1,4 +0,0 @@
-# Vscode  快捷键
-
-1. **Ctrl+Shit+P**: 打开命令
-2. **Markdown: Open Preview to the Side**: Markdown预览
