f1b16c7f0c198c48f7f6605c68bc4dcbf61477d6
裸奔白菜 <bangxw@qq.com>
2019-07-07 22:05:18 +0800

    updateing

--- 08.React/demo/react.itcast/src/index.js
+++ 08.React/demo/react.itcast/src/index.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const myH1 = React.createElement('h1', {
+  id: 'myH1',
+  'title': 'this is a h1'
+}, '这是个H1')
+
+ReactDOM.render(myH1, document.getElementById('app'))
