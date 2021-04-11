06c4c40741c8401e09ce698d1175ebb8c6919f39
裸奔白菜 <bangxw.im@outlook.com>
2021-04-11 21:09:28 +0800

    sync home

---
+++ 07.Vue/pratice/vue-primary/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Instruction />
+</template>
+
+<script>
+import Instruction from './components/instruction/index'
+
+export default {
+  name: 'App',
+  components: {
+    Instruction
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
